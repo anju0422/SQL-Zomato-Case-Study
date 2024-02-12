42862185fbc03cb80dc1d29f8a7a1841afa427a0
Anju Saini <110930875+anju0422@users.noreply.github.com>
2024-02-12 17:34:14 +0530

    Create Zomato-Queries.sql

---
+++ Zomato-Queries.sql
@@ -0,0 +1,100 @@
+    SELECT * FROM goldusers_signup;
+    SELECT * FROM users;
+    SELECT * FROM sales;
+    SELECT * FROM product;
+    
+    
+  /* 1.what is total amount each customer spent on zomato ? */
+  
+  SELECT
+  s.userid,
+  SUM(p.price) AS total_amount
+  FROM sales s 
+  JOIN product p 
+  ON s.product_id = p.product_id
+  GROUP BY s.userid
+  ORDER BY total_amount DESC;
+  
+  /* 2.How many days has each customer visited zomato? */
+  
+  SELECT 
+  userid,
+  COUNT(created_date) AS no_of_days_visited 
+  FROM sales
+  GROUP BY userid 
+  ORDER BY no_of_days_visited DESC;
+  
+  /* 3.what was the first product purchased by each customer? */
+  
+  WITH CTE AS(
+  SELECT 
+  *,
+  DENSE_RANK() OVER(PARTITION BY userid ORDER BY created_date) AS rnk
+  FROM sales
+  )
+     SELECT 
+     c.userid,
+     p.product_name 
+     FROM CTE c 
+     JOIN product p 
+     ON c.product_id = p.product_id 
+     WHERE c.rnk = 1;
+    
+  /* 4. what is most purchased item on menu & how many times was it purchased by all customers ? */  
+  
+  WITH CTE AS(
+  SELECT 
+  s.product_id,
+  p.product_name,
+  COUNT(s.product_id) AS cnt,
+  DENSE_RANK() OVER (ORDER BY COUNT(product_id) DESC) AS rnk
+  FROM sales s
+  JOIN product p
+  ON s.product_id = p.product_id
+  GROUP BY product_id,  p.product_name
+  )
+  
+  SELECT 
+  s.userid,
+  c.product_name,
+  COUNT(c.product_id) cnt_of_items
+  FROM sales s 
+  LEFT JOIN CTE c 
+  ON s.product_id = c.product_id 
+  WHERE c.rnk = 1
+  GROUP BY s.userid, c.product_id, c.product_name
+  ORDER BY cnt_of_items DESC;
+  
+  /* 5. which item was most popular for each customer? */
+  
+    WITH CTE AS(
+    SELECT 
+    s.userid,
+    s.product_id,
+    p.product_name,
+    COUNT(s.product_id) AS cnt,
+    DENSE_RANK() OVER(PARTITION BY s.userid ORDER BY COUNT(s.product_id) DESC) AS rnk
+    FROM sales s
+    JOIN product p
+    ON s.product_id = p.product_id
+    GROUP BY s.userid, s.product_id, p.product_name
+    ORDER BY s.userid, s.product_id, p.product_name
+    )
+    
+    SELECT
+    userid,
+    product_name AS most_popular_product
+    FROM CTE 
+    WHERE rnk = 1;
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
